92b359872255cf45ec25a851002e51bbdd174e75
Taoshif1 <taoshif2@gmail.com>
2026-01-04 14:23:09 +0600

    verifyToken added

---
+++ middleware/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ 
+      success: false, 
+      message: 'No token provided or invalid format' 
+    });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ 
+      success: false, 
+      message: 'Invalid or expired token' 
+    });
+  }
+};
+
+module.exports = verifyToken;
